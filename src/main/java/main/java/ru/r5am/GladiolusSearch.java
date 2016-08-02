f2663c8a517307b5af14572d04d636e29892b429
Alex <richmanfx@yandex.ru>
2016-08-02 22:50:30 +0400

    Добавлена сборка gradle.

---
+++ src/main/java/main/java/ru/r5am/GladiolusSearch.java
@@ -0,0 +1,16 @@
+/**
+ * Created by zoer on 31.07.2016.
+ */
+
+package main.java.ru.r5am;
+
+import org.openqa.selenium.By;
+
+
+public class GladiolusSearch {
+    public static void main(String[] args) {
+        // Драйвер Firefox
+        WebDriver driver = new FirefoxDriver();
+        System.out.println("Привет!");
+    }
+}
