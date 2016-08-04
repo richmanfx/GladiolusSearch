c66cb072438327613042245a07a234b7e5c66567
Alex <richmanfx@yandex.ru>
2016-08-04 22:57:26 +0400

    Запускается FireFox, но не управляется WebDriver - несовместимость версий.

--- src/main/java/main/java/ru/r5am/GladiolusSearch.java
+++ src/main/java/main/java/ru/r5am/GladiolusSearch.java
@@ -5,12 +5,37 @@
 package main.java.ru.r5am;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+//import static com.codeborne.selenide.Selenide.*;
 
 
 public class GladiolusSearch {
     public static void main(String[] args) {
         // Драйвер Firefox
         WebDriver driver = new FirefoxDriver();
-        System.out.println("Привет!");
+//        WebDriver driver = new ChromeDriver();
+
+//        driver.get("http://www.google.com");
+        driver.get("http://www.google.com");
+        //Navigation.to("http://www.google.com");
+
+        WebElement element = driver.findElement(By.name("q"));
+
+        element.sendKeys("Cheese!");
+        element.submit();
+        System.out.println("Page title is: " + driver.getTitle());
+
+        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().startsWith("cheese!");
+            }
+        });
+
+        System.out.println("Page title is: " + driver.getTitle());
+        driver.quit();
     }
 }
